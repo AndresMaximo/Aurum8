1c11221c2007d3c39a7dc1f659346ecca0f6690d
AndresMaximo <andres.marcelo.bello@gmail.com>
2025-08-18 12:21:54 -0400

    Update aurum.js
    ruta

--- aurum.js
+++ aurum.js
@@ -3,7 +3,7 @@
 <script>
   // ⬇⬇ EDITA SOLO ESTAS DOS CONSTANTES ⬇⬇
   const SUPABASE_URL  = "https://awtsikwkfqluobpmqywi.supabase.co";
-  const SUPABASE_ANON = "TU_ANON_KEY_AQUÍ";
+  const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3aXRrc3drdmZxdWJvd3BxbXZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzMTM0MzYsImV4cCI6MjA3MDg4OTQzNn0.sV-unvowZvZP2q7osj5uL_JNpK-X4J2aLTAlrZOm5to";
   // ⬆⬆ EDITA SOLO ESTAS DOS CONSTANTES ⬆⬆
 
   window.db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON);
