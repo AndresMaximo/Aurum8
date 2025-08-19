b561dd36c28cda9040b9e4d4fc9a64cd1626bab4
AndresMaximo <andres.marcelo.bello@gmail.com>
2025-08-18 21:53:43 -0400

    Actualizar superbaseClient.js

--- Modulos/common/superbaseClient.js
+++ Modulos/common/superbaseClient.js
@@ -1,1 +1,9 @@
 
+// /common/supabaseClient.js
+// Reemplaza estas constantes con tus credenciales reales de Supabase
+const SUPABASE_URL = "https://TU-PROYECTO.supabase.co";
+const SUPABASE_ANON_KEY = "TU-ANON-KEY";
+
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
