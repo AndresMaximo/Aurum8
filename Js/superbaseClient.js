682ae2e203bbc0257d3cc553b658a22bee683502
AndresMaximo <andres.marcelo.bello@gmail.com>
2025-08-20 14:56:18 -0400

    Actualizar superbaseClient.js

--- Js/superbaseClient.js
+++ Js/superbaseClient.js
@@ -1,11 +1,12 @@
-<!-- /Js/supabaseClient.js -->
-<script>
-// ⚠️ Este archivo NO debe usar "type=module".
-// Requiere que antes esté cargado el CDN: @supabase/supabase-js v2
+// /js/supabaseClient.js
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const SUPABASE_URL = "https://qwitkswkvfqubowpqmvy.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3aXRrc3drdmZxdWJvd3BxbXZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzMTM0MzYsImV4cCI6MjA3MDg4OTQzNn0.sV-unvowZvZP2q7osj5uL_JNpK-X4J2aLTAlrZOm5to";
 
-// Cliente global: window.sb
-window.sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-</script>
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+  },
+});
