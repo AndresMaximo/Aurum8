65d9c254fbe755268bf3c6d638e47c6758e7938a
AndresMaximo <andres.marcelo.bello@gmail.com>
2025-08-21 16:06:27 -0400

    Actualizar supabaseClient.js

--- Js/supabaseClient.js
+++ Js/supabaseClient.js
@@ -1,8 +1,8 @@
-// /js/supabaseClient.js
+// js/supabaseClient.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "https://qwitkswkvfqubowpqmvy.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3aXRrc3drdmZxdWJvd3BxbXZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzMTM0MzYsImV4cCI6MjA3MDg4OTQzNn0.sV-unvowZvZP2q7osj5uL_JNpK-X4J2aLTAlrZOm5to";
+const SUPABASE_URL = "https://qwitskwkvfqubowpqmyv.supabase.co"; 
+const SUPABASE_ANON_KEY = "eyJh...tu_key_completa..."; 
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
