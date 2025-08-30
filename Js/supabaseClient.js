a59d0cc2dde20928445db7844aaa297a6ffb7822
AndresMaximo <andres.marcelo.bello@gmail.com>
2025-08-30 12:02:13 -0400

    Update supabaseClient.js

--- Js/supabaseClient.js
+++ Js/supabaseClient.js
@@ -1,12 +1,9 @@
-// js/supabaseClient.js
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = "https://qwitskwkvfqubowpqmyv.supabase.co"; 
-const SUPABASE_ANON_KEY = "eyJh...tu_key_completa..."; 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-  },
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  persistSession: true,
+  autoRefreshToken: true,
 });
